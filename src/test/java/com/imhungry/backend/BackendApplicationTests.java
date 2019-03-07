d19d2785b628eec1e949e6c4265aea5af4a02002
Chris Hailey <chris.hailey8@gmail.com>
2019-03-06 23:11:16 -0800

    Added more tests

--- src/test/java/com/imhungry/backend/BackendApplicationTests.java
+++ src/test/java/com/imhungry/backend/BackendApplicationTests.java
@@ -15,44 +15,81 @@ public class BackendApplicationTests {
 	
 	@Test
 	public void testConstructRecipe() {
+		Recipe r = new Recipe(); 
 		
 	}
-	
 	
 	@Test
 	public void testConstructRestaurant() {
-		
+		Restaurant r = new Restaurant(); 
 	}
 	
+	
+	
+	
+	@Test
+	public void testBuildCollage() {
+		CollageController c = new CollageController(); 
+		
+	}
 	@Test
 	public void testCollageSuccess()  {
-		
+		CollageController c = new CollageController(); 
+		c.getCollage("burgers"); 
 	}
 	
-	@Test
-	public void testCollageEmpty()  {
-		
+	public void testCollageTenPhotos() {
+		CollageController c = new CollageController(); 
+		c.getCollage("babaghanoush"); 
 	}
+	
+	public void testCollageNoSearch() {
+		CollageController c = new CollageController(); 
+		c.getCollage(""); 
+	}
+	
 	
 	@Test
 	public void testCollageFail()  {
-		
+		CollageController c = new CollageController(); 
+		c.getCollage("adfbhklaldafjkljash"); 
 	}
 	
 	@Test
 	public void testSearchSuccess() {
+		RecipeSourcer r1 = new RecipeSourcer(); 
+		r1.getRecipe("burgers"); 
 		
-		
-	}
-	
-	@Test 
-	public void testSearchEmpty() {
+		RestaurantSourcer r2  = new RestaurantSourcer(); 
+		r2.searchRestaurants("burgers", 5); 
 		
 	}
 	
 	@Test
-	public void testSearchFail() {
+	public void testSearchMultipleKeywords() {
+		RecipeSourcer r1 = new RecipeSourcer(); 
+		r1.getRecipe("chinese food"); 
 		
+		RestaurantSourcer r2  = new RestaurantSourcer(); 
+		r2.searchRestaurants("chinese food", 5); 
+	}
+	
+	@Test
+	public void testSearchBlank() {
+		RecipeSourcer r1 = new RecipeSourcer(); 
+		r1.getRecipe(""); 
+		
+		RestaurantSourcer r2  = new RestaurantSourcer(); 
+		r2.searchRestaurants("", 5);
+	}
+	
+	@Test
+	public void testSearchFail() {
+		RecipeSourcer r1 = new RecipeSourcer(); 
+		r1.getRecipe("adfbhklaldafjkljash"); 
+		
+		RestaurantSourcer r2  = new RestaurantSourcer(); 
+		r2.searchRestaurants("adfbhklaldafjkljash", 5);		
 		
 	}
 	
@@ -62,10 +99,11 @@ public class BackendApplicationTests {
 	}
 	
 	@Test
-	public void testRemoveFromList() {
+	public void testRemoveFromEmptyList() {
 		
 	}
 	
+
 	
 	
 	
