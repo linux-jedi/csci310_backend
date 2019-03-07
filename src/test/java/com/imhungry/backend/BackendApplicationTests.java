1c41a421868cc2c7a778771a4ba7135809a6022d
Chris Hailey <chris.hailey8@gmail.com>
2019-03-06 18:42:42 -0800

    Changed some testing stuff

--- src/test/java/com/imhungry/backend/BackendApplicationTests.java
+++ src/test/java/com/imhungry/backend/BackendApplicationTests.java
@@ -12,5 +12,65 @@ public class BackendApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+	
+	@Test
+	public void testConstructRecipe() {
+		
+	}
+	
+	
+	@Test
+	public void testConstructRestaurant() {
+		
+	}
+	
+	@Test
+	public void testCollageSuccess()  {
+		
+	}
+	
+	@Test
+	public void testCollageEmpty()  {
+		
+	}
+	
+	@Test
+	public void testCollageFail()  {
+		
+	}
+	
+	@Test
+	public void testSearchSuccess() {
+		
+		
+	}
+	
+	@Test 
+	public void testSearchEmpty() {
+		
+	}
+	
+	@Test
+	public void testSearchFail() {
+		
+		
+	}
+	
+	@Test 
+	public void testAddToList() {
+		
+	}
+	
+	@Test
+	public void testRemoveFromList() {
+		
+	}
+	
+	
+	
+	
+	
 
 }
+
+
