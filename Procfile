67f0145fd10dc9eefddfeb66454d4d64ea610fd3
Shray Pungaliya <pungaliy@usc.edu>
2019-04-07 20:06:42 -0700

    Necessary files untracked

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/backend-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev
-
---
+++ src/main/java/com/imhungry/backend/repository/SearchHistoryRespository.java
@@ -0,0 +1,28 @@
+package com.imhungry.backend.repository;
+
+import com.imhungry.backend.model.SearchQuery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Repository
+public interface SearchHistoryRespository extends JpaRepository<SearchQuery, Long> {
+
+    Collection<SearchQuery> findAllByUserIdOrderByUpdatedAtDesc(long userId);
+
+    @Modifying
+    @Query(value = "update searches s set s.userId = :uid where s.id = :id", nativeQuery = true)
+    void updateSearchHistory (@Param("uid") Long uid,
+                             @Param("id") Long id);
+
+    Optional<SearchQuery> findByUserIdAndSearchTermAndAmountAndRadius
+            (long userId, String searchTerm, int amount, int radius);
+
+    void deleteById(long id);
+
+}
